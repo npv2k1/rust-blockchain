f77b7fef938ab0cc4bc8fef20a2185c6470b6ee7
Phạm Văn Nguyên <npv2k1@gmail.com>
2022-12-04 23:45:12 +0700

    first commit

---
+++ src/blockchain/block.rs
@@ -0,0 +1,9 @@
+// mod blockchain {
+//     pub mod block {
+//         pub struct Block {
+//             pub hash: String,
+//             pub data: String,
+//             pub prevHash: String,
+//         }
+//     }
+// }
